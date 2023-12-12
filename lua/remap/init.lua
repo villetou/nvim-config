65746d7057d97fac36bfab9d1ffeeb5481f2604b
Ville Touronen <touronen@gmail.com>
2023-12-12 23:17:00 +0200

    Better page up and down

--- lua/remap/init.lua
+++ lua/remap/init.lua
@@ -4,4 +4,11 @@ vim.g.mapleader = " "
 vim.keymap.set('', '<C-s>', '<Cmd>w<CR>')
 vim.keymap.set('', '<C-w>w', '<Cmd>bd<CR>')
 
-vim.keymap.set("n", "<Leader>ff", "<CMD>lua vim.lsp.buf.format()<CR>")
+vim.keymap.set('n', "<Leader>ff", "<CMD>lua vim.lsp.buf.format()<CR>")
+
+-- Easier movement
+vim.keymap.set('n', '<C-d>', '<C-d>zz')
+vim.keymap.set('n', '<C-u>', '<C-u>zz')
+-- vim.keymap.set('n', '<M-<Right>>', 'B')
+-- vim.keymap.set('n', '<M-<Left>>', 'E')
+
