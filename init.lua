926b29e7c6730b7be1320a2c4f0a20ab708382b1
Ville Touronen <touronen@gmail.com>
2024-02-12 11:38:29 +0200

    Added default LazyVim configs

--- init.lua
+++ init.lua
@@ -1,3 +1,2 @@
-require('remap')
-require('bootstrap-lazy')
-require('villetou')
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
--- lua/plugins/prisma.lua
+++
@@ -1,6 +0,0 @@
-return {
-	'prisma/vim-prisma',
-	lazy = true,
-	config = function() end,
-	ft = {'prisma'},
-}
